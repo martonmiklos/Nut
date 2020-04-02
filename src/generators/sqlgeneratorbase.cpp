f676efe322e2124239a22524901bc7c0c7c76ac7
Miklós Márton <martonmiklosqdev@gmail.com>
2020-04-02 22:52:39 +0200

    - Implemented sum field feature - Fixed SingleField typo

--- src/generators/sqlgeneratorbase.cpp
+++ src/generators/sqlgeneratorbase.cpp
@@ -545,7 +545,10 @@ QString SqlGeneratorBase::agregateText(const AgregateType &t,
     case Count:
         return "COUNT(" + arg + ")";
 
-    case SignleField:
+    case Sum:
+        return "SUM(" + arg + ")";
+
+    case SingleField:
         return arg;
     }
     return QString(); // never reach
